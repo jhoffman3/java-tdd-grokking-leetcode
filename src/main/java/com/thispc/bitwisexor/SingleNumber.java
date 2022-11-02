1dd378e066153c818d8be69cd80541bbc435d009
Jon Hoffman <30374104+jhoffman3@users.noreply.github.com>
2022-11-01 17:19:45 -0700

    Update SingleNumber.java

--- src/main/java/com/thispc/bitwisexor/SingleNumber.java
+++ src/main/java/com/thispc/bitwisexor/SingleNumber.java
@@ -12,7 +12,7 @@ public class SingleNumber {
 
         int answer = nums[0];
         for (int i = 1; i < nums.length; i++) {
-            answer^=nums[i];
+            answer ^= nums[i];
         }
         return answer;
     }
